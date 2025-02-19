3e6b8c8785d42bf70ea799ac479d25a4eadbc6f7
Dare345 <ydarren615@gmail.com>
2025-02-19 00:12:24 -0500

    Academic Projects and exercise I did for ISTE 140

---
+++ ISTE 140/demos/PHP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello World<br/>
+
+    <?php
+        $word = "Welcome to RIT";
+        echo "<strong>Hello World From PHP</strong>" . " "
+
+    ?>
+        <br/>
+    
+</body>
+</html>
